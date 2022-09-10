70f0ebb9e15ae6e54bf9e171f4dcbb41525084bc
Nathan Mayhook <jnmayhook@gmail.com>
2022-09-09 17:34:52 -0700

    added login, signup, search sets forms to react, CSS

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,13 +6,17 @@ function Footer() {
   return (
     <footer className="footer text-center mt-auto">
       <div className="container d-flex justify-content-between">
-        <img className="footer-bg" src={footerBg} alt="BricksRepo logo" />
+      <img className="footer-bg" src={footerBg} alt="BricksRepo logo" />
         <ul className="footer-nav d-flex flex-row align-items-center">
           <li className="px-3"><Link to="/">Home</Link></li>
           <li className="px-3"><Link to="/LoginForm">Login</Link></li>
           <li className="px-3"><Link to="/SignupForm">Signup</Link></li>
           <li className="px-3 copyright">&copy; 2022 BricksRepo</li>
         </ul>
+        </div>
+        <div className="container d-flex flex-column mt-5">
+        <p>The building blocks that power the Bricks Repo App are thoughtfully shared by Brickset, one of the most established, accurate, and up-to-date LEGO databases on the Internet today: they've been collating data since 1997. Brickset's database contains information on 18992 LEGO sets and other items released over the last 73 years.</p>
+        <p>LEGO, the LEGO logo, the Minifigure, and the Brick and Knob configurations are trademarks of the LEGO Group of Companies. ©2022 The LEGO Group.</p>
       </div>
     </footer>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,37 +3,62 @@ import React from 'react';
 const Home = () => {
     return (
         <>
-        <div id="myCarousel" className="carousel slide carousel-fade" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true"
-                    aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <div className="slide-one-bg" aria-hidden="true"></div>
+            <div id="myCarousel" className="carousel slide carousel-fade mb-5" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true"
+                        aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <div className="slide-one-bg" aria-hidden="true"></div>
 
-                    <div className="container">
-                        <div className="carousel-caption text-start">
-                            <img className="" src=""
-                                alt="" />
-                            <p></p>
+                        <div className="container">
+                            <div className="carousel-caption text-start">
+                                <img className="" src=""
+                                    alt="" />
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className="slide-two-bg" aria-hidden="true"></div>
+
+                        <div className="container">
+                            <div className="carousel-caption text-start">
+                                <img className="" src=""
+                                    alt="" />
+                                <p></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="carousel-item">
-                    <div className="slide-two-bg" aria-hidden="true"></div>
-
-                    <div className="container">
-                        <div className="carousel-caption text-start">
-                            <img className="" src=""
-                                alt="" />
-                            <p></p>
-                        </div>
+            </div>
+            <div className="container mb-5 mb-153">
+            <div className='row'>
+                <h1 className='mb-3'>Search for your favorite Lego Sets!</h1>
+                <p>Type something into the search box to find the sets you're interested in</p>
+                <div className='col-7'>
+                <form onSubmit={""} className="needs-validation" novalidate>
+                    <input
+                        type='text'
+                        name='search_sets'
+                        class='form-control form-control-lg' 
+                        placeholder='Enter Set Name'
+                        value={""}
+                        onChange={"handleChange"}
+                        required
+                    />
+                    <div className="invalid-feedback">
+                        Please enter a valid email.
                     </div>
+
+                    <button type="submit" class="btn btn-blue mt-3">Search</button>
+                    
+                </form>
+                </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
