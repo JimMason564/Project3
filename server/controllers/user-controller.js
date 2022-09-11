2c7b501a6e7dd12ae1627414cf3a5216d43fb94f
Nathan Mayhook <jnmayhook@gmail.com>
2022-09-10 17:38:26 -0700

    updated user-controller

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -42,14 +42,14 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+  // save a set to a user's `savedSets` field by adding it to the set (to prevent duplicates)
   // user comes from `req.user` created in the auth middleware function
   async saveSet({ user, body }, res) {
     console.log(user);
     try {
       const updatedUser = await User.findOneAndUpdate(
         { _id: user._id },
-        { $addToSet: { savedBooks: body } },
+        { $addToSet: { savedSets: body } },
         { new: true, runValidators: true }
       );
       return res.json(updatedUser);
@@ -58,11 +58,11 @@ module.exports = {
       return res.status(400).json(err);
     }
   },
-  // remove a book from `savedBooks`
+  // remove a set from `savedSets`
   async deleteSet({ user, params }, res) {
     const updatedUser = await User.findOneAndUpdate(
       { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
+      { $pull: { savedSets: { setId: params.setId } } },
       { new: true }
     );
     if (!updatedUser) {
