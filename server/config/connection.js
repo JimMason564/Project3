3edfed43450bf3536a9079986cc69e08c4f3de37
Jim Mason <jimmason564@yahoo.com>
2022-09-16 19:40:05 -0700

    Correction

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/legosearch', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/legoProject",
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
-.then(() => console.log('connected'))
-.catch(e => console.log(e))
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://test1234:20sNZCUO4IbVz5mc@legoproject.hbkqcsh.mongodb.net/?retryWrites=true&w=majority",
-// {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+  useCreateIndex: true
+});
 
 module.exports = mongoose.connection;
+
+
+// mongodb://127.0.0.1:27017/legoProject
+
+// mongodb+srv://test1234:20sNZCUO4IbVz5mc@legoproject.hbkqcsh.mongodb.net/?retryWrites=true&w=majority
